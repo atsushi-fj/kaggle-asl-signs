1b66b09d56d6a6a576e4c2e6e862a107c641b65e
atsus-fj <atsushi061452@icloud.com>
2023-03-21 06:06:33 +0900

    update gru 3

--- src/model_builder/backborn/gru.py
+++ src/model_builder/backborn/gru.py
@@ -6,17 +6,29 @@ class GRU(nn.Module):
     def __init__(self,
                  input_size):
         super().__init__()
-        self.gru = nn.GRU(input_size, 64, 3,
-                          dropout=0.1,
-                          batch_first=True)
-        self.dense1 = nn.Linear(64, 64)
+        self.gru1 = nn.GRU(input_size, 64,
+                           dropout=0.1,
+                           batch_first=True)
+        self.gru2 = nn.GRU(64, 128,
+                           dropout=0.1,
+                           batch_first=True)
+        self.gru3 = nn.GRU(128, 64,
+                           dropout=0.1,
+                           batch_first=True)
+        self.flatten = nn.Flatten(1, -1)
+        self.dense1 = nn.Linear(543*64, 64)
         self.dense2 = nn.Linear(64, 32)
         self.dense3 = nn.Linear(32, 250)
         self.relu = nn.ReLU()
     
+    
     def forward(self, x):
-        x, _ = self.gru(x)
+        x, _ = self.gru1(x)
         x = self.relu(x)
+        x, _ = self.gru2(x)
+        x = self.relu(x)
+        x, _ = self.gru3(x)
+        x = self.flatten(self.relu(x))
         x = self.relu(self.dense1(x))
         x = self.relu(self.dense2(x))
         x = self.relu(self.dense3(x))
