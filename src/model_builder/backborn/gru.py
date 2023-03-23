caf3fbe09e32e6819a2133e48088ad78d9379dc6
atsus-fj <atsushi061452@icloud.com>
2023-03-23 20:26:04 +0900

    update gru

--- src/model_builder/backborn/gru.py
+++ src/model_builder/backborn/gru.py
@@ -1,27 +1,33 @@
 import torch.nn as nn
+from torch.nn import functional as F
 from torchinfo import summary
 
 
 class GRU(nn.Module):
     def __init__(self,
-                 input_size=[32, 543, 3],
-                 dropout_rate=0.1):
+                 input_size=3,
+                 dropout_rate=0.1,
+                 n_layers=3):
         super().__init__()
-        self.input_size = input_size
-        self.gru = nn.GRU(self.input_size[-1], 560, 3,
+        self.gru = nn.GRU(input_size, 560, n_layers,
                            dropout=dropout_rate,
                            batch_first=True)
-        self.dense1 = nn.Linear(560, 560)
-        self.dense2 = nn.Linear(560, 280)
-        self.dense3 = nn.Linear(280, 250)
-        self.relu = nn.ReLU()
+        self.dense = nn.Sequential(
+            nn.Linear(560, 560),
+            nn.ReLU()
+        )
+        self.dense1 = self.dense * (n_layers-2)
+        self.dense2 = nn.Sequential(
+            nn.Linear(560, 280),
+            nn.ReLU(),
+            nn.Linear(280, 250)
+        )
     
     
     def forward(self, x):
         x, _ = self.gru(x)
-        x = self.relu(self.dense1(x[:, -1, :]))
-        x = self.relu(self.dense2(x))
-        x = self.relu(self.dense3(x))
+        x = self.dense1(x[:, -1, :])
+        x = self.dense2(x)
         return x
 
 
