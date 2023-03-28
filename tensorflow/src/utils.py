d368077935b2615dd4c28fd8c320423954d057b1
atsus-fj <atsushi061452@icloud.com>
2023-03-28 14:51:27 +0900

    update tensorflow

--- tensorflow/src/utils.py
+++ tensorflow/src/utils.py
@@ -2,19 +2,22 @@ from pathlib import Path
 import yaml
 import tensorflow as tf
 import numpy as np
+import math
 
 # Custom callback to update weight decay with learning rate
 class WeightDecayCallback(tf.keras.callbacks.Callback):
-    def __init__(self, wd_ratio=cfg.WD_RATIO):
+    def __init__(self, model, cfg):
+        self.model = model
         self.step_counter = 0
-        self.wd_ratio = wd_ratio
+        self.wd_ratio = cfg.WD_RATIO
     
     def on_epoch_begin(self, epoch, logs=None):
-        model_one.optimizer.weight_decay = model_one.optimizer.learning_rate * self.wd_ratio
-        print(f'learning rate: {model_one.optimizer.learning_rate.numpy():.2e}, weight decay: {model_one.optimizer.weight_decay.numpy():.2e}')
+        self.model.optimizer.weight_decay = self.model.optimizer.learning_rate * self.wd_ratio
+        print(f'learning rate: {self.model.optimizer.learning_rate.numpy():.2e}, weight decay: {self.model.optimizer.weight_decay.numpy():.2e}')
 
 
-def lrfn(current_step, num_warmup_steps, lr_max, num_cycles=0.50, num_training_steps=cfg.N_EPOCHS):
+def lrfn(current_step, num_warmup_steps, lr_max, cfg, num_cycles=0.50):
+    num_training_steps=cfg.N_EPOCHS
     
     if current_step < num_warmup_steps:
         if WARMUP_METHOD == 'log':
@@ -25,47 +28,7 @@ def lrfn(current_step, num_warmup_steps, lr_max, num_cycles=0.50, num_training_s
         progress = float(current_step - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
 
         return max(0.0, 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress))) * lr_max
-
-
-def plot_lr_schedule(lr_schedule, epochs):
-    fig = plt.figure(figsize=(20, 10))
-    plt.plot([None] + lr_schedule + [None])
-    # X Labels
-    x = np.arange(1, epochs + 1)
-    x_axis_labels = [i if epochs <= 40 or i % 5 == 0 or i == 1 else None for i in range(1, epochs + 1)]
-    plt.xlim([1, epochs])
-    plt.xticks(x, x_axis_labels) # set tick step to 1 and let x axis start at 1
     
-    # Increase y-limit for better readability
-    plt.ylim([0, max(lr_schedule) * 1.1])
-    
-    # Title
-    schedule_info = f'start: {lr_schedule[0]:.1E}, max: {max(lr_schedule):.1E}, final: {lr_schedule[-1]:.1E}'
-    plt.title(f'Step Learning Rate Schedule, {schedule_info}', size=18, pad=12)
-    
-    # Plot Learning Rates
-    for x, val in enumerate(lr_schedule):
-        if epochs <= 40 or x % 5 == 0 or x is epochs - 1:
-            if x < len(lr_schedule) - 1:
-                if lr_schedule[x - 1] < val:
-                    ha = 'right'
-                else:
-                    ha = 'left'
-            elif x == 0:
-                ha = 'right'
-            else:
-                ha = 'left'
-            plt.plot(x + 1, val, 'o', color='black');
-            offset_y = (max(lr_schedule) - min(lr_schedule)) * 0.02
-            plt.annotate(f'{val:.1E}', xy=(x + 1, val + offset_y), size=12, ha=ha)
-    
-    plt.xlabel('Epoch', size=16, labelpad=5)
-    plt.ylabel('Learning Rate', size=16, labelpad=5)
-    plt.grid()
-    plt.show()
-
-
-
 
 def load_config(file="config.yaml"):
     """Load config file"""
