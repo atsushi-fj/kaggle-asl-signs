620a21973bf455d71554089ecdced9d72d4e2f14
atsus-fj <atsushi061452@icloud.com>
2023-04-05 11:56:08 +0900

    update config

--- tensorflow/src/model_builder/get_model/get_gru.py
+++ tensorflow/src/model_builder/get_model/get_gru.py
@@ -19,7 +19,7 @@ def get_gru(cfg):
     
     x = GRU(cfg)(x)
     x = tf.keras.layers.Softmax(dtype="float32")(x)
-    outputs = tf.reshape(x, (-1))
+    outputs = tf.reshape(x, (250, -1))
     
     model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
     loss = "sparse_categorical_crossentropy"
