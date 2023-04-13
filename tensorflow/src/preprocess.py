fe85d792724b94fd64c9077b0f01fd2a09a65a19
atsus-fj <atsushi061452@icloud.com>
2023-04-13 12:11:02 +0900

    update new process

--- tensorflow/src/preprocess.py
+++ tensorflow/src/preprocess.py
@@ -90,4 +90,157 @@ class PreprocessLayer(tf.keras.layers.Layer):
             data = tf.where(tf.math.is_nan(data), 0.0, data)
             
             return data, non_empty_frames_idxs
+        
+
+class PreprocessLayerDominant(tf.keras.layers.Layer):
+    def __init__(self, cfg):
+        super().__init__()
+        
+        USE_TYPES = ['left_hand', 'pose', 'right_hand']
+        START_IDX = 468
+        self.LIPS_IDXS0 = np.array([
+                61, 185, 40, 39, 37, 0, 267, 269, 270, 409,
+                291, 146, 91, 181, 84, 17, 314, 405, 321, 375,
+                78, 191, 80, 81, 82, 13, 312, 311, 310, 415,
+                95, 88, 178, 87, 14, 317, 402, 318, 324, 308,
+            ])
+        # Landmark indices in original data
+        self.LEFT_HAND_IDXS0 = np.arange(468,489)
+        self.RIGHT_HAND_IDXS0 = np.arange(522,543)
+        self.LEFT_POSE_IDXS0 = np.array([502, 504, 506, 508, 510])
+        self.RIGHT_POSE_IDXS0 = np.array([503, 505, 507, 509, 511])
+
+        self.LANDMARK_IDXS_LEFT_DOMINANT0 = np.concatenate((self.LIPS_IDXS0, self.LEFT_HAND_IDXS0, self.LEFT_POSE_IDXS0))
+        self.LANDMARK_IDXS_RIGHT_DOMINANT0 = np.concatenate((self.LIPS_IDXS0, self.RIGHT_HAND_IDXS0, self.RIGHT_POSE_IDXS0))
+        self.HAND_IDXS0 = np.concatenate((self.LEFT_HAND_IDXS0, self.RIGHT_HAND_IDXS0), axis=0)
+        self.N_COLS = self.LANDMARK_IDXS_LEFT_DOMINANT0.size
+        
+        # Landmark indices in processed data
+        LIPS_IDXS = np.argwhere(np.isin(self.LANDMARK_IDXS_LEFT_DOMINANT0, self.LIPS_IDXS0)).squeeze()
+        LEFT_HAND_IDXS = np.argwhere(np.isin(self.LANDMARK_IDXS_LEFT_DOMINANT0, self.LEFT_HAND_IDXS0)).squeeze()
+        RIGHT_HAND_IDXS = np.argwhere(np.isin(self.LANDMARK_IDXS_LEFT_DOMINANT0, self.RIGHT_HAND_IDXS0)).squeeze()
+        HAND_IDXS = np.argwhere(np.isin(self.LANDMARK_IDXS_LEFT_DOMINANT0, self.HAND_IDXS0)).squeeze()
+        POSE_IDXS = np.argwhere(np.isin(self.LANDMARK_IDXS_LEFT_DOMINANT0, self.LEFT_POSE_IDXS0)).squeeze()
+            
+        normalisation_correction = tf.constant([
+                    # Add 0.50 to left hand (original right hand) and substract 0.50 of right hand (original left hand)
+                    [0] * len(LIPS_IDXS) + [0.50] * len(LEFT_HAND_IDXS) + [0.50] * len(POSE_IDXS),
+                    # Y coordinates stay intact
+                    [0] * len(self.LANDMARK_IDXS_LEFT_DOMINANT0),
+                    # Z coordinates stay intact
+                    [0] * len(self.LANDMARK_IDXS_LEFT_DOMINANT0),
+                ],
+                dtype=tf.float32,
+            )
+        self.normalisation_correction = tf.transpose(normalisation_correction, [1,0])
+        self.cfg = cfg
+        
+    def pad_edge(self, t, repeats, side):
+        if side == 'LEFT':
+            return tf.concat((tf.repeat(t[:1], repeats=repeats, axis=0), t), axis=0)
+        elif side == 'RIGHT':
+            return tf.concat((t, tf.repeat(t[-1:], repeats=repeats, axis=0)), axis=0)
+    
+    @tf.function(
+        input_signature=(tf.TensorSpec(shape=[None, 543, 3], dtype=tf.float32),),)
+    def call(self, data0):
+        # Number of Frames in Video
+        N_FRAMES0 = tf.shape(data0)[0]
+        
+        # Find dominant hand by comparing summed absolute coordinates
+        left_hand_sum = tf.math.reduce_sum(tf.where(tf.math.is_nan(tf.gather(data0, LEFT_HAND_IDXS0, axis=1)), 0, 1))
+        right_hand_sum = tf.math.reduce_sum(tf.where(tf.math.is_nan(tf.gather(data0, RIGHT_HAND_IDXS0, axis=1)), 0, 1))
+        left_dominant = left_hand_sum >= right_hand_sum
+        
+        # Count non NaN Hand values in each frame for the dominant hand
+        if left_dominant:
+            frames_hands_non_nan_sum = tf.math.reduce_sum(
+                    tf.where(tf.math.is_nan(tf.gather(data0, self.LEFT_HAND_IDXS0, axis=1)), 0, 1),
+                    axis=[1, 2],
+                )
+        else:
+            frames_hands_non_nan_sum = tf.math.reduce_sum(
+                    tf.where(tf.math.is_nan(tf.gather(data0, self.RIGHT_HAND_IDXS0, axis=1)), 0, 1),
+                    axis=[1, 2],
+                )
+        
+        # Find frames indices with coordinates of dominant hand
+        non_empty_frames_idxs = tf.where(frames_hands_non_nan_sum > 0)
+        non_empty_frames_idxs = tf.squeeze(non_empty_frames_idxs, axis=1)
+
+        # Filter frames
+        data = tf.gather(data0, non_empty_frames_idxs, axis=0)
+        
+        # Cast Indices in float32 to be compatible with Tensorflow Lite
+        non_empty_frames_idxs = tf.cast(non_empty_frames_idxs, tf.float32)
+        
+        # Normalize to start with 0
+        non_empty_frames_idxs -= tf.reduce_min(non_empty_frames_idxs)
+        
+        # Number of Frames in Filtered Video
+        N_FRAMES = tf.shape(data)[0]
+        
+        # Gather Relevant Landmark Columns
+        if left_dominant:
+            data = tf.gather(data, self.LANDMARK_IDXS_LEFT_DOMINANT0, axis=1)
+        else:
+            data = tf.gather(data, self.LANDMARK_IDXS_RIGHT_DOMINANT0, axis=1)
+            data = (
+                    self.normalisation_correction + (
+                        (data - self.normalisation_correction) * tf.where(self.normalisation_correction != 0, -1.0, 1.0))
+                )
+        
+        # Video fits in INPUT_SIZE
+        if N_FRAMES < self.cfg.INPUT_SIZE:
+            # Pad With -1 to indicate padding
+            non_empty_frames_idxs = tf.pad(non_empty_frames_idxs, [[0, self.cfg.INPUT_SIZE-N_FRAMES]], constant_values=-1)
+            # Pad Data With Zeros
+            data = tf.pad(data, [[0, self.cfg.INPUT_SIZE-N_FRAMES], [0,0], [0,0]], constant_values=0)
+            # Fill NaN Values With 0
+            data = tf.where(tf.math.is_nan(data), 0.0, data)
+            return data, non_empty_frames_idxs
+        # Video needs to be downsampled to INPUT_SIZE
+        else:
+            # Repeat
+            if N_FRAMES < self.cfg.INPUT_SIZE**2:
+                repeats = tf.math.floordiv(self.cfg.INPUT_SIZE * self.cfg.INPUT_SIZE, N_FRAMES0)
+                data = tf.repeat(data, repeats=repeats, axis=0)
+                non_empty_frames_idxs = tf.repeat(non_empty_frames_idxs, repeats=repeats, axis=0)
+
+            # Pad To Multiple Of Input Size
+            pool_size = tf.math.floordiv(len(data), self.cfg.INPUT_SIZE)
+            if tf.math.mod(len(data), self.cfg.INPUT_SIZE) > 0:
+                pool_size += 1
+
+            if pool_size == 1:
+                pad_size = (pool_size * self.cfg.INPUT_SIZE) - len(data)
+            else:
+                pad_size = (pool_size * self.cfg.INPUT_SIZE) % len(data)
+
+            # Pad Start/End with Start/End value
+            pad_left = tf.math.floordiv(pad_size, 2) + tf.math.floordiv(self.cfg.INPUT_SIZE, 2)
+            pad_right = tf.math.floordiv(pad_size, 2) + tf.math.floordiv(self.cfg.INPUT_SIZE, 2)
+            if tf.math.mod(pad_size, 2) > 0:
+                pad_right += 1
+
+            # Pad By Concatenating Left/Right Edge Values
+            data = self.pad_edge(data, pad_left, 'LEFT')
+            data = self.pad_edge(data, pad_right, 'RIGHT')
+
+            # Pad Non Empty Frame Indices
+            non_empty_frames_idxs = self.pad_edge(non_empty_frames_idxs, pad_left, 'LEFT')
+            non_empty_frames_idxs = self.pad_edge(non_empty_frames_idxs, pad_right, 'RIGHT')
+
+            # Reshape to Mean Pool
+            data = tf.reshape(data, [self.cfg.INPUT_SIZE, -1, self.cfg.N_COLS, self.cfg.N_DIMS])
+            non_empty_frames_idxs = tf.reshape(non_empty_frames_idxs, [self.cfg.INPUT_SIZE, -1])
+
+            # Mean Pool
+            data = tf.experimental.numpy.nanmean(data, axis=1)
+            non_empty_frames_idxs = tf.experimental.numpy.nanmean(non_empty_frames_idxs, axis=1)
+
+            # Fill NaN Values With 0
+            data = tf.where(tf.math.is_nan(data), 0.0, data)
+            
+            return data, non_empty_frames_idxs
     
