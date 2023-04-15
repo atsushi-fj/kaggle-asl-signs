ed6b93b6646792f6a5f973552baaeb18370f5272
atsus-fj <atsushi061452@icloud.com>
2023-04-15 10:51:27 +0900

    update transformer

--- tensorflow/src/model_builder/get_model/get_gru.py
+++ tensorflow/src/model_builder/get_model/get_gru.py
@@ -4,6 +4,15 @@ from ...utils import load_input64_data, get_lr_metric
 from ..gru import GRU
 
 
+def scce_with_ls(y_true, y_pred):
+    # One Hot Encode Sparsely Encoded Target Sign
+    y_true = tf.cast(y_true, tf.int32)
+    y_true = tf.one_hot(y_true, 250, axis=1)
+    y_true = tf.squeeze(y_true, axis=2)
+    # Categorical Crossentropy with native label smoothing support
+    return tf.keras.losses.categorical_crossentropy(y_true, y_pred, label_smoothing=0.25)
+
+
 def get_gru(cfg):
     X, y, _ = load_input64_data()
     
@@ -14,7 +23,7 @@ def get_gru(cfg):
     outputs = GRU(cfg)(x)
     
     model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
-    loss = "sparse_categorical_crossentropy"
+    loss = scce_with_ls
     
     # Adam Optimizer with weight decay
     optimizer = tfa.optimizers.AdamW(learning_rate=cfg.lr,
@@ -22,6 +31,7 @@ def get_gru(cfg):
                                      clipnorm=cfg.clipnorm)
     
     lr_metric = get_lr_metric(optimizer)
+    
     metrics = ["acc",lr_metric]
     model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
     return X, y, model
