8eaccb5e03a467094d6a2cb48aad223e49439fba
atsus-fj <atsushi061452@icloud.com>
2023-04-15 10:43:52 +0900

    update transformer

--- tensorflow/src/model_builder/transformer/transformer.py
+++ tensorflow/src/model_builder/transformer/transformer.py
@@ -45,12 +45,8 @@ class Transformer(tf.keras.Model):
         self.mlps = []
         # Make Transformer Blocks
         for i in range(self.num_blocks):
-            # First Layer Normalisation
-            self.ln_1s.append(tf.keras.layers.LayerNormalization(epsilon=self.cfg.LAYER_NORM_EPS))
             # Multi Head Attention
             self.mhas.append(MultiHeadAttention(self.cfg.UNITS, 8))
-            # Second Layer Normalisation
-            self.ln_2s.append(tf.keras.layers.LayerNormalization(epsilon=self.cfg.LAYER_NORM_EPS))
             # Multi Layer Perception
             self.mlps.append(tf.keras.Sequential([
                 tf.keras.layers.Dense(self.cfg.UNITS * self.cfg.MLP_RATIO,
@@ -62,13 +58,9 @@ class Transformer(tf.keras.Model):
         
     def call(self, x, attention_mask):
         # Iterate input over transformer blocks
-        for ln_1, mha, ln_2, mlp in zip(self.ln_1s, self.mhas, self.ln_2s, self.mlps):
-            x1 = ln_1(x)
-            attention_output = mha(x1, attention_mask)
-            x2 = x1 + attention_output
-            x3 = ln_2(x2)
-            x3 = mlp(x3)
-            x = x3 + x2
+        for mha, mlp in zip(self.mhas, self.mlps):
+            x = x + mha(x, attention_mask)
+            x = x + mlp(x)
     
         return x
 
