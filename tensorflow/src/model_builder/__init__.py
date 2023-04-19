81cb6e37a159bb935c7c564c1780a1717015ef8a
atsus-fj <atsushi061452@icloud.com>
2023-04-19 22:48:57 +0900

    update gru

--- tensorflow/src/model_builder/__init__.py
+++ tensorflow/src/model_builder/__init__.py
@@ -1,2 +1,2 @@
 from .get_model.get_transformer import get_transformer
-from .get_model.get_gru import get_gru, get_feature_gru
+from .get_model.get_gru import get_gru, get_feature_gru, get_new_feature_gru
--- tensorflow/src/model_builder/get_model/get_gru.py
+++ tensorflow/src/model_builder/get_model/get_gru.py
@@ -96,3 +96,61 @@ def get_feature_gru(cfg):
     metrics = ["acc",lr_metric]
     model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
     return X, y, model
+
+
+def get_new_feature_gru(cfg):
+    X, y, _ = load_input64_data()
+    feature_stats = create_feature_statistics_input64(X)
+    
+    inputs = tf.keras.layers.Input(shape=(cfg.INPUT_SIZE, cfg.N_COLS, cfg.N_DIMS),
+                                   dtype=tf.float32,
+                                   name="inputs")
+    
+    x = tf.slice(inputs, [0,0,0,0], [-1, cfg.INPUT_SIZE, cfg.N_COLS, 2])
+    
+    # LIPS
+    lips = tf.slice(x, [0,0,feature_stats["lips"][0],0], [-1,cfg.INPUT_SIZE, 40, 2])
+    lips = tf.where(
+            tf.math.equal(lips, 0.0),
+            0.0,
+            (lips - feature_stats["lips"][1]) / feature_stats["lips"][2],
+        )
+    lips = tf.reshape(lips, [-1, cfg.INPUT_SIZE, 40*2])
+    
+    # LEFT HAND
+    left_hand = tf.slice(x, [0,0,40,0], [-1,cfg.INPUT_SIZE, 21, 2])
+    left_hand = tf.where(
+            tf.math.equal(left_hand, 0.0),
+            0.0,
+            (left_hand - feature_stats["left_hand"][1]) / feature_stats["left_hand"][2],
+        )
+    left_hand = tf.reshape(left_hand, [-1, cfg.INPUT_SIZE, 21*2])
+    
+    # POSE
+    pose = tf.slice(x, [0,0,61,0], [-1,cfg.INPUT_SIZE, 5, 2])
+    pose = tf.where(
+            tf.math.equal(pose, 0.0),
+            0.0,
+            (pose - feature_stats["pose"][1]) / feature_stats["pose"][2],
+        )
+    pose = tf.reshape(pose, [-1, cfg.INPUT_SIZE, 5*2])
+    
+    x = tf.keras.layers.Concatenate(axis=1)([lips, left_hand, pose])
+    
+    x = GRU(x)
+    
+    outputs = mlp_block(x, cfg)
+    
+    model = tf.keras.models.Model(inputs=inputs, outputs=outputs)
+    loss = scce_with_ls
+    
+    # Adam Optimizer with weight decay
+    optimizer = tfa.optimizers.AdamW(learning_rate=cfg.lr,
+                                     weight_decay=cfg.weight_decay,
+                                     clipnorm=cfg.clipnorm)
+    
+    lr_metric = get_lr_metric(optimizer)
+    
+    metrics = ["acc",lr_metric]
+    model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
+    return X, y, model
