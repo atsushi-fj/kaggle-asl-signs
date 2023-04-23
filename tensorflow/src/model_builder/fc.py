0921873bf5486c7a8154321a834b36afad84f1cb
atsus-fj <atsushi061452@icloud.com>
2023-04-23 15:20:58 +0900

    update fc

--- tensorflow/src/model_builder/fc.py
+++ tensorflow/src/model_builder/fc.py
@@ -3,25 +3,22 @@ import tensorflow as tf
 
 class FC(tf.keras.Model):
     def __init__(self, cfg):
-        super().__init__()
-        self.start_fc = tf.keras.layers.Dense(cfg.UNITS, activation="relu")
-        
+        super().__init__()        
         self.end_fc = tf.keras.layers.Dense(cfg.NUM_CLASSES,
                                             activation="softmax")
         
-        if (cfg.NUM_BLOCKS - 2) > 0:
+        if (cfg.NUM_BLOCKS - 1) > 0:
             self.fc_blocks = [
                 tf.keras.layers.Dense(cfg.UNITS),
                 tf.keras.layers.BatchNormalization(),
                 tf.keras.layers.ReLU(),
-                tf.keras.layers.Dropout(cfg.MLP_DROPOUT_RATE, seed=cfg.SEED),] * (cfg.NUM_BLOCKS - 2)
+                tf.keras.layers.Dropout(cfg.MLP_DROPOUT_RATE, seed=cfg.SEED),] * (cfg.NUM_BLOCKS - 1)
             self.flag_use_fc_block = True
         else:
             self.flag_use_fc_block = False
             
     def get_config(self):
         config = {
-            "start_fc" : self.start_fc,
             "end_fc" : self.end_fc,
             "flag_use_fc_block" : self.flag_use_fc_block
         }
@@ -29,12 +26,8 @@ class FC(tf.keras.Model):
         return dict(list(base_config.items()) + list(config.items()))
 
     def call(self, x):
-        # x_tmp = 0
-        x = self.start_fc(x)
         if self.flag_use_fc_block:
             for blk in self.fc_blocks:
-                # x += x_tmp
-                # x_tmp = x
                 x = blk(x)
         x = self.end_fc(x)
         return x
