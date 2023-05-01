541bd71455c454064b272016cdca33a9a6595f66
atsus-fj <atsushi061452@icloud.com>
2023-05-02 01:30:08 +0900

    update convlstm

--- tensorflow/src/model_builder/conv1dlstm.py
+++ tensorflow/src/model_builder/conv1dlstm.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 
 def conv1d_lstm_block(inputs, filters):
-    vector = tf.keras.layers.ConvLSTM1D(filters=8, kernel_size=2)(inputs)
+    vector = tf.keras.layers.ConvLSTM1D(filters=16, kernel_size=2)(inputs)
     for f in filters:
         vector = tf.keras.layers.Conv1D(filters=f, kernel_size=2)(vector)
         vector = tf.keras.layers.MaxPooling1D()(vector)
